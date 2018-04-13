546c43d76a356d9ffedd361143006664ec0a741a
n1ck <qserxsdiv@gmail.com>
2018-04-13 19:36:40 +0700

    Initial commit

---
+++ app/src/main/java/com/deepholesdetection/n1ck/core/main.kt
@@ -0,0 +1,2 @@
+package com.deepholesdetection.n1ck.core
+
---
+++ app/src/main/java/com/deepholesdetection/n1ck/deepholesdetection/CameraCaptureActivity.kt
@@ -0,0 +1,12 @@
+package com.deepholesdetection.n1ck.deepholesdetection
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraCaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_capture)
+    }
+}
